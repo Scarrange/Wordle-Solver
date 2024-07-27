ec800ddd89438a9f41bcbb3e95d93e2b2f50aed9
Scarrange <xMizor@gmail.com>
2024-07-27 17:54:33 +0200

    Works fine

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,12 @@
 const shadowRoot = document.querySelector("game-app").shadowRoot;
 const board = shadowRoot.getElementById("board");
-console.log("Shadow Root", shadowRoot);
-console.log("Board", board);
 const gameRows = board.querySelectorAll("game-row");
-console.log("Game Rows", gameRows);
-console.log("End");
+const gameTiles = [[], [], [], [], []];
+for (let i = 0; i < 5; i++) {
+  gameTiles[i] = Array.from(
+    gameRows[i].shadowRoot.querySelectorAll("game-tile")
+  );
+}
 
 const sleep = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -25,27 +27,85 @@ const sendKey = (key) => {
   document.dispatchEvent(new KeyboardEvent("keydown", { key, bubbles: true }));
 };
 
-const tryWords = async (words) => {
-  for (const word of words) {
-    console.log("Word:", word);
-    word.split("").forEach((letter) => {
-      sendKey(letter);
-    });
-    sendKey("Enter");
-    await sleep(2000);
+const tryWord = async (word) => {
+  console.log("Word:", word);
+  word.split("").forEach((letter) => {
+    sendKey(letter);
+  });
+  sendKey("Enter");
+  await sleep(2000);
+};
+
+const removeWord = (wordList, word) => {
+  const index = wordList.indexOf(word);
+  console.log("index: ", index);
+  if (index < 0) {
+    return;
   }
+  wordList.splice(index, 1);
+};
+
+const createRegex = (letter, index) => {
+  const dots = Array.from({ length: index }, () => ".").join("");
+  return new RegExp(`^${dots}${letter}`);
+};
+
+const analyzeWord = (index) => {
+  const checks = [];
+  gameTiles[index].forEach((tile, index) => {
+    if (tile.getAttribute("evaluation") === "correct") {
+      checks.push(createRegex(tile.getAttribute("letter"), index));
+    }
+    if (tile.getAttribute("evaluation") === "present") {
+      checks.push(new RegExp(tile.getAttribute("letter")));
+    }
+    if (tile.getAttribute("evaluation") === "absent") {
+      checks.push(createRegex(`[^${tile.getAttribute("letter")}]`, index));
+    }
+  });
+  return checks;
+};
+
+const filterWords = (allWords, checks) => {
+  return allWords.filter((word) => {
+    return checks.every((check) => {
+      return word.match(check);
+    });
+  });
 };
 
 const main = async () => {
   const base = ["seoul", "train"];
   const takenGuesses = [];
-  SOLUTION_WORDS = await readFile("/util/wordle_words.json");
-  const FREQUENCIES = await readFile("/util/frequencies.json");
-  console.log("Solutions", SOLUTION_WORDS);
-  console.log("Frequencies", FREQUENCIES);
+  const checks = [];
+  let counter = 0;
+  let guess = "";
+  let solutionWords = await readFile("/util/wordle_words.json");
 
-  await tryWords(base);
-  takenGuesses.push(...base);
-  console.log(takenGuesses);
+  while (counter < 2) {
+    guess = base[counter];
+    await tryWord(guess);
+    takenGuesses.push(guess);
+    removeWord(solutionWords, guess);
+    checks.push(...analyzeWord(counter));
+    console.log("guesses taken: ", takenGuesses);
+    console.log("checks: ", checks);
+    solutionWords = filterWords(solutionWords, checks);
+    console.log("solutionWords: ", solutionWords);
+    counter++;
+  }
+
+  while (counter < 6) {
+    guess = solutionWords[0];
+    await tryWord(guess);
+    takenGuesses.push(guess);
+    removeWord(solutionWords, guess);
+    checks.push(...analyzeWord(counter));
+    console.log("guesses taken: ", takenGuesses);
+    console.log("checks: ", checks);
+    solutionWords = filterWords(solutionWords, checks);
+    console.log("solutionWords: ", solutionWords);
+    counter++;
+  }
 };
 main();
